aaf486a2cbcde21f51d8b0aa55aa737308496946
Aurel Subescu <aurelsubescu@live.com>
2022-12-18 00:21:41 +0200

    Add project files.

---
+++ FriendlyBet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FriendlyBet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
