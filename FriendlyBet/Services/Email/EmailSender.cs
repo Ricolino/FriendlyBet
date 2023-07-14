bc4627a6f2c7a60e74fb0ac6283dd23aecd4894f
Ricolino <aurelsubescu@live.com>
2023-07-14 20:45:09 +0300

    Partial commit

---
+++ FriendlyBet/Services/Email/EmailSender.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace FriendlyBet.Services.Email
+{
+    public class EmailSender:IEmailSender
+    {
+        private readonly ILogger _logger;
+        public AuthMessageSenderOptions Options { get; } //Set with Secret Manager.
+
+        public EmailSender(IOptions<AuthMessageSenderOptions> optionsAccessor,
+                       ILogger<EmailSender> logger)
+        {
+            Options = optionsAccessor.Value;
+            _logger = logger;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            if (string.IsNullOrEmpty(Options.SendGridKey))
+            {
+                throw new Exception("Null SendGridKey");
+            }
+            await Execute(Options.SendGridKey, subject, htmlMessage, email);
+        }
+
+        public async Task Execute(string apiKey, string subject, string message, string toEmail)
+        {
+            var client = new SendGridClient(apiKey);
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress("no-reply@friendlybet.com", "Password Recovery"),
+                Subject = subject,
+                PlainTextContent = message,
+                HtmlContent = message
+            };
+            msg.AddTo(new EmailAddress(toEmail));
+
+            // Disable click tracking.
+            // See https://sendgrid.com/docs/User_Guide/Settings/tracking.html
+            msg.SetClickTracking(false, false);
+            var response = await client.SendEmailAsync(msg);
+            _logger.LogInformation(response.IsSuccessStatusCode
+                                   ? $"Email to {toEmail} queued successfully!"
+                                   : $"Failure Email to {toEmail}");
+        }
+    }
+}
